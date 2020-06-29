1fcaae2983e1e4002c8928a1dac1556405a975c3
vijaysingh188 <dunchanmaster007@gmail.com>
2020-06-30 02:28:52 +0530

    billing checkout

--- Ecommerce/amazon/amazon/settings.py
+++ Ecommerce/amazon/amazon/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #our app
     'accounts',
+    'billing',
     'carts',
     'orders',
     'products',
--- Ecommerce/amazon/carts/views.py
+++ Ecommerce/amazon/carts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from . models import Cart
 from products.models import Product
 from orders.models import Order
-
+from billing.models import BillingProfile
+from accounts.forms import LoginForm
 
 
 #  Create your views here.
@@ -37,7 +38,21 @@ def checkout_home(request):
         return redirect("cart:home")
     else:
         order_obj,new_order_obj = Order.objects.get_or_create(cart=cart_obj)
-    return render(request,'carts/checkout_home.html',{'object':order_obj})
+
+    user = request.user
+    login_form = LoginForm()
+    billing_profile = None
+    if user.is_authenticated():
+
+        billing_profile,billing_profile_create = BillingProfile.objects.get_or_create(user=user,email=user.email)
+
+    context = {
+        'object':order_obj,
+        'billing_profile': billing_profile,
+        'login_form': login_form,
+    }
+    print(billing_profile,'billing_profile')
+    return render(request,'carts/checkout_home.html',context)
 
 
 
